39003ca8ebeccea0a5d9f5cae1f316266d747d9c
Aleshere <alessioburgio85@gmail.com>
2021-05-11 09:38:54 -0400

    first commit

---
+++ src/components/footer/footer.data.js
@@ -0,0 +1,24 @@
+export default {
+  menuItem: [
+    {
+      path: '/',
+      label: 'Home',
+    },
+    {
+      path: '/',
+      label: 'Advertise',
+    },
+    {
+      path: '/',
+      label: 'Supports',
+    },
+    {
+      path: '/',
+      label: 'Marketing',
+    },
+    {
+      path: '/',
+      label: 'Contact',
+    },
+  ],
+};
